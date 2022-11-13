03ede59159c181afb6a77f368e006b558dbebd37
YusufEminAkpinar <yakpinar137@gmail.com>
2022-11-13 20:24:59 +0300

    Cleaning up code a little bit

--- youtube_links.py
+++ youtube_links.py
@@ -2,38 +2,21 @@ from re import findall
 from urllib.request import urlopen
 
 
+def toSec(time):
+    iki_nokta = time.find(':')
+    return int(time[:iki_nokta]) * 60 + int(time[iki_nokta + 1:])
+
+def toMin(time):
+    return f'{time // 60}:{time % 60}'
+
+
 def time_substractor(time1, time2):
-    iki_nokta_1 = time1.find(':')
-    iki_nokta_2 = time2.find(':')
-    minute1 = int(time1[:iki_nokta_1])
-    second1 = int(time1[iki_nokta_1 + 1:])
-    minute2 = int(time2[:iki_nokta_2])
-    second2 = int(time2[iki_nokta_2 + 1:])
-    if minute2 > minute1:
-        pass
-    elif minute2 == minute1:
-        if second2 > second1:  # make time2 bigger time always
-            pass
-        else:
-            tmp = second1
-            second1 = second2
-            second2 = tmp
-            temp = minute1
-            minute1 = minute2
-            minute2 = temp
-    else:  # make time2 bigger time always
-        tmp = second1
-        second1 = second2
-        second2 = tmp
-        temp = minute1
-        minute1 = minute2
-        minute2 = temp
-    minute = minute2 - minute1
-    second = second2 - second1
-    if second < 0:
-        minute -= 1
-        second += 60
-    return abs(minute)
+    toSec1 = toSec(time1)
+    toSec2 = toSec(time2)
+    if toSec2 < toSec1: # make real_time2 bigger time always
+        toSec1, toSec2 = toSec2, toSec1
+    return toMin(toSec2 - toSec1)
+
 
 def songs_youtube_id(liste, real_times):
     lst = []
@@ -43,7 +26,7 @@ def songs_youtube_id(liste, real_times):
           'please be patient...')
     print('-----------------------------------------------------------------------------------------------------------\n')
     p = 0
-    avec_probleme = []
+    problems = []
     # for song_name in liste:
     while i < len(liste):
         song_name = liste[i]
@@ -56,41 +39,9 @@ def songs_youtube_id(liste, real_times):
         video_times_list = []
         for element in video_times_tuple_without_empty:
             video_times_list.append(f'{element[0]}:{element[1]}')
-        try:  # checking video times.
-            if time_substractor(real_times[i], video_times_list[0]) < 1:
-                print(f'babuş ilk yere girdi\nspotify:{real_times[i]}\nyoutube:{video_times_list[0]}')
-                lst.append(video_ids[0])
-                print(f'{song_name}: {video_ids[0]}')
-                print('------------------------------------\n')
-                p = 0
-            elif time_substractor(real_times[i], video_times_list[1]) < 1:
-                print(f'babuş ikinci yere girdi\nspotify:{real_times[i]}\nyoutube:{video_times_list[1]}')
-                lst.append(video_ids[1])
-                print(f'{song_name}: {video_ids[1]}')
-                print('------------------------------------\n')
-                p = 0
-            elif time_substractor(real_times[i], video_times_list[2]) < 1:
-                print(f'babuş üçüncü yere girdi\nspotify:{real_times[i]}\nyoutube:{video_times_list[2]}')
-                lst.append(video_ids[2])
-                print(f'{song_name}: {video_ids[2]}')
-                print('------------------------------------\n')
-                p = 0
-            else:
-                print(video_times_list, '\n')
-                print('kendisi:', real_times[i])
-                print('Kardeş bir sorun var bu şarkıda kb.\n')
-                p += 1
-                if p < 4:
-                    i -= 1
-                else:
-                    avec_probleme.append(song_name)
-        except IndexError:
-            print('AGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-        except ValueError:
-            print(video_times_tuple, '\n')
-            print(video_times_list, '\n')
-            print('real_time:', real_times[i])
-            print('video_time:', video_times_list, '\n')
         i += 1
+        print(f'Video ids: {video_ids[0]}')
+        lst.append(video_ids[0])
     return lst
+
 
